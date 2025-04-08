469b3e6a3cdf40e8cb3c2742a2cad7139a2184e1
robertofabiot <rftercerom@uamv.edu.ni>
2025-04-08 10:33:41 -0600

    Nomenclatura 3

---
+++ Nomenclatura3.py
@@ -0,0 +1,13 @@
+# Declara nombres de variables para los siguientes casos, aplicando la convención 
+# indicada. 
+# 1. Nombre completo del estudiante – camelCase 
+# 2. Número de matrícula – snake_case 
+# 3. Total de ventas del mes – PascalCase 
+# 4. Límite máximo de intentos – SCREAMING_SNAKE_CASE 
+# 5. Cantidad de productos – camelCase
+
+nombreCompletoEstudiante = "Roberto Fabio Tercero Membreño"
+numero_matricula = 9890282
+totalVentasMes = 0
+LIMITE_MAXIMO_INTENTOS = 1
+cantidadProductos = 8
