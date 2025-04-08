e7775b0ba3aacbaf5ce385b8fd094cb9fdf80c70
robertofabiot <rftercerom@uamv.edu.ni>
2025-04-08 10:21:44 -0600

    Act 2 ejercicio 7

---
+++ Act2ejercicio7.py
@@ -0,0 +1,9 @@
+#Diseña un algoritmo que intercambie el valor de dos variables numéricas. Usa una 
+#variable auxiliar para hacerlo. 
+variable1 = 1
+variable2 = 2
+auxiliar = None
+
+auxiliar = variable1
+variable1 = variable2
+variable2 = auxiliar
