4d7bf96ce4a5d511a7ffa94e503f20409784b1cd
robertofabiot <rftercerom@uamv.edu.ni>
2025-04-08 11:19:41 -0600

    Act 4 ejercicios 11 y 12

---
+++ Act4ejercicio11.py
@@ -0,0 +1,7 @@
+# Calcular el número de pulsaciones que una persona debe tener por cada 10 segundos 
+# de ejercicio, si la fórmula es
+
+edad = int(input("Introduce tu edad: "))
+numero_pulsaciones = (220-edad)/10
+
+print(f"El número de pulsaciones que debes tener por cada 10 segundos de ejercicio es: {numero_pulsaciones}")
---
+++ Act4ejercicio12.py
@@ -0,0 +1,8 @@
+# El dueño de una tienda compra un artículo a un precio determinado. Obtener el precio 
+# en que lo debe vender para obtener una ganancia del 30%. 
+
+precio_compra = float(input("Ingrese el precio de compra del artículo: "))
+precio_venta = precio_compra * 1.3
+
+print(f"El precio de venta para obtener una ganancia del 30% es: {precio_venta:.2f}")
+
