989825c6f08ce3bcd84d2b6a4d687abb8f662379
robertofabiot <rftercerom@uamv.edu.ni>
2025-04-08 10:39:18 -0600

    Nomenclatura 4

---
+++ Nomenclatura4.py
@@ -0,0 +1,13 @@
+# A partir de las siguientes descripciones, escribe el nombre de una variable que siga 
+# una convención lógica y clara.  Puedes elegir la convención, pero justificarla. 
+# 1. La edad del usuario 
+# 2. El precio total del carrito de compras 
+# 3. El estado de inicio de sesión (activo o inactivo) 
+# 4. El nombre del curso actual 
+# 5. La temperatura promedio registrada 
+
+edadUsuario = 8 # camel case
+precio_total_carrito = 100 # snake case
+EstadoInicioSesion = True # pascal case
+NOMBRE_CURSO_ACTUAL = "Introducción a la programación" # screaming snake case
+temperaturaPromedio = 25 # camel case
