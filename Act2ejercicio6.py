89f34840e041bd4404d3d41ff16b172cd91fa261
robertofabiot <rftercerom@uamv.edu.ni>
2025-04-08 10:18:29 -0600

    Act 2 ejercicio 6

---
+++ Act2ejercicio6.py
@@ -0,0 +1,6 @@
+año_actual = 2025
+año_nacimiento = int(input("Introduce tu año de nacimiento: "))
+edad_actual = año_actual - año_nacimiento
+edad_diezaños = edad_actual + 10
+print("Tu edad actual es:", edad_actual, "años")
+print("Tu edad dentro de diez años será:", edad_diezaños, "años")
