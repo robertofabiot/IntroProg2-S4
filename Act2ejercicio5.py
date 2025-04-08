bfceee7f9a36d3a6aa2c35d3e64d9595bc2c7a71
robertofabiot <rftercerom@uamv.edu.ni>
2025-04-08 10:15:54 -0600

    ejercicio 5

---
+++ Act2ejercicio5.py
@@ -0,0 +1,4 @@
+numero = float(input("Introduce el número: "))
+porcentaje = float(input("Introduce el porcentaje: "))
+resultado = numero * (porcentaje / 100)
+print("El resultado es:", resultado, "%")
