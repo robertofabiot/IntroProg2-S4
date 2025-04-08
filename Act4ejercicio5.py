fc28187a9c447dc61f57ea457e7a6643889c7f7a
robertofabiot <rftercerom@uamv.edu.ni>
2025-04-08 10:59:11 -0600

    Act 4 ejercicio 5

---
+++ Act4ejercicio5.py
@@ -0,0 +1,7 @@
+# Dado un total de cuenta y un porcentaje de propina (por ejemplo, 10%), calcula cuánto 
+# dejar de propina.
+
+total_cuenta = float(input("Introduce el total de la cuenta: "))
+propina = total_cuenta * 0.1
+
+print("La propina es:", propina)
